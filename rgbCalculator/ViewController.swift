954e9c3f36d5f54d85170d807d39d614b6709bc9
Artem <artem-ermak-696@yande.by>
2021-03-29 20:27:39 +0300

    Fixed titles

--- rgbCalculator/ViewController.swift
+++ rgbCalculator/ViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    @IBOutlet var redSliderTitle: UILabel!
+    @IBOutlet var greenSliderTitle: UILabel!
+    @IBOutlet var blueSliderTitle: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    func updateColor(){
+        let red = CGFloat(redSlider.value)
+        let green = CGFloat(greenSlider.value)
+        let blue = CGFloat(blueSlider.value)
+        
+        let color = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        colorView.backgroundColor = color
+    }
+    @IBAction func sliderChanged() {
+        redSliderTitle.text = String(format: "%.2f", redSlider.value)
+        greenSliderTitle.text = String(format: "%.2f", greenSlider.value)
+        blueSliderTitle.text = String(format: "%.2f", blueSlider.value)
+        updateColor()
+    }
+    
 }
 
